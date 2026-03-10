443081655896a55c185a08614487285cbd66ba26
LuK1337 <priv.luk@gmail.com>
2026-03-10 09:12:28 +0100

    sm8250-common: Remove fwk-detect lib from PRODUCT_PACKAGES
    See: Iac75d83e6bc46c137c328a2365e6701c210bfe7a
    Change-Id: I6b8571e81e59f438e34b76823f47ff024ec33dc6

--- common.mk
+++ common.mk
@@ -272,10 +272,6 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
-# QTI fwk-detect
-PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
-
 # RealmeAddons
 $(call inherit-product-if-exists, packages/apps/RealmeAddons/realmeaddons.mk)
 
